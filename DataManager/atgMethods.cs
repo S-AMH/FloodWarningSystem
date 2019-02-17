6478912832c5036293dc5096ff97f4fbe0b05405
S.AmirMohammadHasanli <hasnali.amir@gmail.com>
2019-02-17 11:39:00 +0330

    Minor Fix in Comments format

--- DataManager/atgMethods.cs
+++ DataManager/atgMethods.cs
@@ -18,7 +18,7 @@ namespace DataManager
         public string xDigitNum(int _number, int _digitsCount)
         {
             if (_number.ToString().Length < _digitsCount)
-                throw new ArgumentException("Error: Operation is not valid. digitCounts is lower than number \'s length.", "_digitCount");
+                throw new ArgumentException("Error: Operation is not valid. digitCounts is lower than number's length.", "_digitCount");
 
             string result = "";
             for (int i = 0; i < _number.ToString().Length - _digitsCount; i++)
