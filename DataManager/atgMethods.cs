51dd47eaaca6a80fcb0339853c327a4fedcd3776
S.AmirMohammadHasanli <hasnali.amir@gmail.com>
2019-02-17 18:25:39 +0330

    Bug Fixed: xDigitNumber() Method was returning invalid values and invalid exceptions.

--- DataManager/atgMethods.cs
+++ DataManager/atgMethods.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DataManager
 {
-    public class atgMethods
+    static public class atgMethods
     {
         /// <summary>
         ///  Converts an integer to string with fixed digit count by adding zeros to left.
@@ -15,13 +15,13 @@ namespace DataManager
         /// <param name="_digitsCount">Digit Counts for Corresponding integer.</param>
         /// <returns>String Corresponding to _number integer with _digitCount digits.</returns>
         /// <exception cref="System.ArgumentException">Thrown When <c>_number</c> digit counts is lower than <c>_digitsCount</c></exception>
-        public string xDigitNum(int _number, int _digitsCount)
+        static public string xDigitNum(int _number, int _digitsCount)
         {
-            if (_number.ToString().Length < _digitsCount)
+            if (_number.ToString().Length > _digitsCount)
                 throw new ArgumentException("Error: Operation is not valid. digitCounts is lower than number's length.", "_digitCount");
 
             string result = "";
-            for (int i = 0; i < _number.ToString().Length - _digitsCount; i++)
+            for (int i = 0; i < _digitsCount - _number.ToString().Length; i++)
                 result += "0";
             return result + _number.ToString();
         }
