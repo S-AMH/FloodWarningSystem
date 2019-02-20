d23e413ffe8038ec486fb608a627619a1de9f943
S.AmirMohammadHasanli <hasnali.amir@gmail.com>
2019-02-20 12:06:30 +0330

    Add Constructors and documents for DownloadRequest class.

--- DataManager/DownloadRequest.cs
+++ DataManager/DownloadRequest.cs
@@ -13,9 +13,62 @@ namespace DataManager
     {
         private Uri uri = new Uri("");
         private string path = "";
+        private int timeLimit = -1;
         //<username, password> -> username:password
         private Tuple<string, string> authentication = new Tuple<string, string>("","");
-        private int timeLimit = -1;
+
+        /// <summary>
+        /// Creates DownloadRequest object with specific download address and path, no authentication is used and timeLimit is set to its maximum (approximately 25 days)
+        /// </summary>
+        /// <param name="uri"> file address in network/internter(url)</param>
+        /// <param name="path"> local path to save downloaded file from network/internet </param>
+        public DownloadRequest(Uri uri, string path)
+        {
+            this.uri = uri;
+            this.path = path;
+            this.timeLimit = int.MaxValue;
+        }
+
+        /// <summary>
+        /// Creates DownloadRequest object with specific download address, path and time limit(Millisecond), no authentication is used.
+        /// </summary>
+        /// <param name="uri">file address in network/internter(url)</param>
+        /// <param name="path">local path to save downloaded file from network/internet</param>
+        /// <param name="timeLimit">unsigned integer value, represented allowed time for file to download in Millisecond</param>
+        public DownloadRequest(Uri uri, string path, int timeLimit)
+        {
+            this.uri = uri;
+            this.path = path;
+            if (timeLimit > 0)
+                this.timeLimit = timeLimit;
+            else
+                throw new ArgumentException("timeLimit should be only unsigned integers and positive values.", "timeLimit");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="uri"></param>
+        /// <param name="path"></param>
+        /// <param name="authentication"></param>
+        public DownloadRequest(Uri uri, string path, Tuple<string, string> authentication)
+        {
+            this.uri = uri;
+            this.path = path;
+            this.authentication = authentication;
+            this.timeLimit = int.MaxValue;
+        }
+
+        public DownloadRequest(Uri uri, string path, Tuple<string, string> authentication, int timeLimit)
+        {
+            this.uri = uri;
+            this.path = path;
+            this.authentication = authentication;
+            if (timeLimit > 0)
+                this.timeLimit = timeLimit;
+            else
+                throw new ArgumentException("timeLimit should be only unsigned integers and positive values.", "timeLimit");
+        }
 
         public Uri Uri { get => uri; set => uri = value; }
         public string Path { get => path; set => path = value; }
