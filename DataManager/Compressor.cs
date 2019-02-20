d1de44c9027ef4435f97824aaba0e75747a6a3a0
S.AmirMohammadHasanli <hasnali.amir@gmail.com>
2019-02-20 15:58:02 +0330

    Add Compressor Class to project. using System.IO.Compression for in/out compressions.

---
+++ DataManager/Compressor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DataManager
+{
+    partial class Compressor
+    {
+        private enum compressorEngine { GZipStream, DeflateStream }
+
+    }
+    partial class Compressor
+    {
+        private List<FileInfo> targetFiles = new List<FileInfo>();
+        private List<DirectoryInfo> targetDirectories = new List<DirectoryInfo>();
+        private string destination = "";
+    }
+}
+
+
