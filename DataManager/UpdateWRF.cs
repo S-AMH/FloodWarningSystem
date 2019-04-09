603534aa94dbfbfd85ba9570b3b05880b4fb29c3
S.AmirMohammadHasanli <hasnali.amir@gmail.com>
2019-04-09 10:08:51 +0430

    Update Handler for WRF DB.

---
+++ DataManager/UpdateWRF.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataManager
+{
+
+    /// <summary>
+    /// This Class Manages all processes needded for updating WRF DataBase. Including pre and post processes.
+    /// </summary>
+    public static class UpdateWRF
+    {
+        public static bool update()
+        {
+
+            return true;
+        }
+    }
+}
